68992176ccd17aff7bcf6c5ac0f46f7943dedc5e
weslley matos <weslley_mattosibf@hotmail.com>
2023-04-04 16:58:38 -0300

    feat: add logic for github client using it for the useRepos hook

---
+++ src/api/github.ts
@@ -0,0 +1,8 @@
+import { Octokit } from "@octokit/core";
+
+
+const token = import.meta.env.GITHUB_TOKEN;
+
+const octokit: Octokit = new Octokit({ auth: token });
+
+export default octokit;
--- src/hooks/useRepos.ts
+++ src/hooks/useRepos.ts
@@ -1,15 +1,25 @@
 import { Dispatch, SetStateAction, useEffect, useState } from "react";
+import { githubClient } from "../api";
 
 const useRepos = (amount: number) => {
   const [allRepos, setAllRepos] = useState<any>([]);
   const [repositories, setRepositories] = useState<any>([]);
 
+  const randomSince = Math.floor(Math.random() * 1000000);
+
   const fetchRepos = async (
     amount: number,
     setters: Dispatch<SetStateAction<any[]>>[],
   ) => {
-    const response = { data: ["repo1", "repo2", "repo3"] }
-    setters.forEach(setter => setter(response.data));
+    const response = await githubClient.request("GET /search/repositories", {
+      q: `is:public stars:>1000 fork:false`,
+      sort: "stars",
+      order: "desc",
+      per_page: amount,
+      since: randomSince,
+      page: 1,
+    })
+    setters.forEach(setter => setter(response.data.items));
   };
 
   useEffect(() => {
