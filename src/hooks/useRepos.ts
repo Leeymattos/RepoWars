a01b6c2d02c9628db5c0fbd0543f787601f0bd21
weslley matos <weslley_mattosibf@hotmail.com>
2023-04-04 17:10:24 -0300

    feat: add types for Repos

--- src/hooks/useRepos.ts
+++ src/hooks/useRepos.ts
@@ -1,26 +1,32 @@
 import { Dispatch, SetStateAction, useEffect, useState } from "react";
 import { githubClient } from "../api";
 
-const useRepos = (amount: number) => {
-  const [allRepos, setAllRepos] = useState<any>([]);
-  const [repositories, setRepositories] = useState<any>([]);
+const randomSince = Math.floor(Math.random() * 1000000);
 
-  const randomSince = Math.floor(Math.random() * 1000000);
+const fetchRepos = async (
+  amount: number,
+  setters: Dispatch<SetStateAction<any[]>>[],
+) => {
+  const response = await githubClient.request("GET /search/repositories", {
+    q: `is:public stars:>1000 fork:false`,
+    sort: "stars",
+    order: "desc",
+    per_page: amount,
+    since: randomSince,
+    page: 1,
+  })
+  setters.forEach(setter => setter(response.data.items));
+};
 
-  const fetchRepos = async (
-    amount: number,
-    setters: Dispatch<SetStateAction<any[]>>[],
-  ) => {
-    const response = await githubClient.request("GET /search/repositories", {
-      q: `is:public stars:>1000 fork:false`,
-      sort: "stars",
-      order: "desc",
-      per_page: amount,
-      since: randomSince,
-      page: 1,
-    })
-    setters.forEach(setter => setter(response.data.items));
-  };
+type UseReposReturnType = [
+  repositories: Repo[],
+  setRepositories: Dispatch<SetStateAction<Repo[]>>,
+  allRepos: Repo[],
+]
+
+const useRepos = (amount: number): UseReposReturnType => {
+  const [allRepos, setAllRepos] = useState<Repo[]>([]);
+  const [repositories, setRepositories] = useState<Repo[]>([]);
 
   useEffect(() => {
     fetchRepos(amount, [setAllRepos, setRepositories]);
