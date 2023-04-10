8f1a63545b132fd495727dc452946fff7196ebdc
weslley matos <weslley_mattosibf@hotmail.com>
2023-04-10 13:57:05 -0300

    feat: add Game logic

--- src/components/game.tsx
+++ src/components/game.tsx
@@ -1,5 +1,5 @@
 import React, { useEffect, useState } from "react"
-import { FaExchangeAlt } from "react-icons/fa"
+import { FaCheckCircle, FaExchangeAlt, FaTimesCircle } from "react-icons/fa"
 import './game.css'
 import RepoCard from './RepoCard'
 
@@ -11,9 +11,44 @@ interface GameProps {
 
 export default function Game({ repoState, originalList, setShowGame }: GameProps) {
 
+
+  const [repositories, setRepositories] = repoState
   const [over, setOver] = useState(false)
   const [repo1, setRepo1] = useState<Repo | null>(null)
   const [repo2, setRepo2] = useState<Repo | null>(null)
+  const [correct, setCorrect] = useState(0)
+  const [wrong, setWrong] = useState(0)
+
+  const randomSelect = (arr: Repo[]) => {
+    if (arr.length < 2) {
+      setRepositories(originalList)
+      setOver(true)
+      return
+    }
+
+    const generateRandomIndex = () => Math.floor(Math.random() * arr.length)
+
+    const radomIndex1 = generateRandomIndex()
+    let randomIndex2 = generateRandomIndex()
+
+    while (randomIndex2 === radomIndex1) {
+      randomIndex2 = generateRandomIndex()
+    }
+
+    const randomRepo1 = arr[radomIndex1]
+    const randomRepo2 = arr[randomIndex2]
+
+    const updatedArr = arr.filter((repo) => repo.id !== randomRepo1.id && repo.id !== randomRepo2.id)
+
+    setRepo1(randomRepo1)
+    setRepo2(randomRepo2)
+    setRepositories(updatedArr)
+  }
+
+  useEffect(() => {
+    randomSelect(repositories)
+  }, [])
+
 
   useEffect((
   ) => {
@@ -21,7 +56,18 @@ export default function Game({ repoState, originalList, setShowGame }: GameProps
     setRepo2(originalList[1])
   }, [])
 
-  const handleChoice = (repo: Repo) => { }
+  const handleChoice = (repo: Repo) => {
+    const chosenRepo = repo.id == repo1?.id ? repo1 : repo2
+    const winner = repo1?.stargazere_count! > repo2?.stargazere_count! ? repo1 : repo2
+
+    if (chosenRepo?.id === winner?.id) {
+      setCorrect(prev => prev + 1)
+    } else {
+      setWrong(prev => prev + 1)
+    }
+    randomSelect(repositories)
+  }
+
   return (
     <div className="game">
       {over ? (<div className="score" > </div>) : (
@@ -30,9 +76,21 @@ export default function Game({ repoState, originalList, setShowGame }: GameProps
             <h1 className="title">Choose the Repo with most Stars</h1>
             <div className="container">
               <RepoCard content={repo1} handler={handleChoice} />
+
               <div className="dashboard">
+                <div className="result corret">
+                  <p>{correct}</p>
+                  <FaCheckCircle />
+                </div>
+
                 <FaExchangeAlt className="icon-versus" />
+
+                <div className="result wrong">
+                  <p>{wrong}</p>
+                  <FaTimesCircle />
+                </div>
               </div>
+
               <RepoCard content={repo2} handler={handleChoice} />
             </div>
           </div>
@@ -51,7 +109,7 @@ export default function Game({ repoState, originalList, setShowGame }: GameProps
           <button
             type="submit"
             className="button gradient"
-            onClick={() => setShowGame(false)}>
+            onClick={() => randomSelect(repositories)}>
             Skip
           </button>
         )}
